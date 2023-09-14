fcfd4ac047d2d578dfa75ccfd3e1473951e7e297
Bartłomiej "Magnetic-Fox" Węgrzyn <magnetic-fox@hotmail.com>
2023-09-14 22:11:57 +0200

    Add resources

---
+++ resources.h
@@ -0,0 +1,17 @@
+#ifndef RESOURCES_H
+#define RESOURCES_H
+
+#include <windows.h>
+
+#define IDI_ICON1                       100
+
+#define IDR_ACCELERATORS                200
+#define ID_ACC_TAB                      201
+#define ID_ACC_ALTF4                    202
+#define ID_ACC_F1                       203
+
+#define IDR_MENU1                       300
+#define ID_PLIK_ZAKOCZ                  301
+#define ID_POMOC_OPROGRAMIE             302
+
+#endif
