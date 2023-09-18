cd2901e717e36b13bf8ecfa8bca45bb204a141c2
Bartłomiej "Magnetic-Fox" Węgrzyn <magnetic-fox@hotmail.com>
2023-09-19 01:16:45 +0200

    Add necessary resources

--- resources.h
+++ resources.h
@@ -10,6 +10,13 @@
 #define ID_ACC_TAB                      201
 #define ID_ACC_ALTF4                    202
 #define ID_ACC_F1                       203
+#define ID_ACC_CTRLN                    204
+#define ID_ACC_ENTER                    205
+#define ID_ACC_F8                       206
+#define ID_ACC_F5                       207
+#define ID_ACC_CTRLA                    208
+#define ID_ACC_CTRLS                    209
+#define ID_ACC_CTRLALTN                 210
 
 #define IDR_MENU1                       300
 #define ID_FILE_NEW                     301
@@ -36,5 +43,16 @@
 #define IDC_STATIC7                     410
 
 #define IDD_DIALOG2                     500
+
+#define IDR_MENU2                       600
+#define ID_FILE_ADDUP                   601
+#define ID_FILE_PROPERTIES              602
+#define ID_FILE_TONEWNOTE               603
+#define ID_EDIT_UNDO                    604
+#define ID_EDIT_CUT                     605
+#define ID_EDIT_COPY                    606
+#define ID_EDIT_PASTE                   607
+#define ID_EDIT_CLEAR                   608
+#define ID_EDIT_SELECTALL               609
 
 #endif
