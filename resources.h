190220e491fa3211a6922598480c5ce77b5d99b7
Bartłomiej "Magnetic-Fox" Węgrzyn <magnetic-fox@hotmail.com>
2023-09-18 16:10:02 +0200

    Add about dialog and update main menu

--- resources.h
+++ resources.h
@@ -11,8 +11,16 @@
 #define ID_ACC_F1                       203
 
 #define IDR_MENU1                       300
-#define ID_FILE_EXIT                    301
-#define ID_HELP_ABOUT                   302
+#define ID_FILE_NEW                     301
+#define ID_FILE_OPEN                    302
+#define ID_FILE_RELOAD                  303
+#define ID_FILE_DELETE                  304
+#define ID_FILE_EXIT                    305
+#define ID_OPTIONS_PREFERENCES          306
+#define ID_OPTIONS_CONNECTION           307
+#define ID_OPTIONS_CREDENTIALS          308
+#define ID_HELP_HELP                    309
+#define ID_HELP_ABOUT                   310
 
 #define IDD_DIALOG1                     400
 #define IDC_BUTTON1                     401
@@ -24,5 +32,8 @@
 #define IDC_STATIC4                     407
 #define IDC_STATIC5                     408
 #define IDC_STATIC6                     409
+#define IDC_STATIC7                     410
+
+#define IDD_DIALOG2                     500
 
 #endif
