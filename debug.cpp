e724e063dc31c90b71e75c1c627beef538935548
Bartłomiej "Magnetic-Fox" Węgrzyn <magnetic-fox@hotmail.com>
2023-09-19 19:46:29 +0200

    Move ShowInteger to debug part (debug sources created)

---
+++ debug.cpp
@@ -0,0 +1,9 @@
+#include "debug.hpp"
+
+void ShowInteger(long int integer)
+{
+    char test[20];
+    ltoa(integer,test,10);
+    MessageBox(0,test,"ShowInteger",MB_OK);
+    return;
+}
---
+++ debug.hpp
@@ -0,0 +1,9 @@
+#ifndef DEBUG_H
+#define DEBUG_H
+
+#include <stdlib.h>
+#include <windows.h>
+
+void ShowInteger(long int);
+
+#endif
