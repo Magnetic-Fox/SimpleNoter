d1f46071cc0d26e529bad2a69a75a3301981d370
Bartłomiej "Magnetic-Fox" Węgrzyn <magnetic-fox@hotmail.com>
2023-09-19 21:46:00 +0200

    Add forgotten (by Watcom developers) checkbox states to constants header

--- constants.hpp
+++ constants.hpp
@@ -1,6 +1,9 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 
+#define BST_UNCHECKED       0x0000
+#define BST_CHECKED         0x0001
+
 #define MATCH_VERSION   "1.0"
 #define USER_AGENT      "SimpleNoter/0.4"
 #define INI_FILE        "config.ini"
