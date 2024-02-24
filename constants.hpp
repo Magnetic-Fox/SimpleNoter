6dd2d7ba043202d9309ec1030629928f84343ae3
Bartłomiej "Magnetic-Fox" Węgrzyn <magnetic-fox@hotmail.com>
2024-02-24 14:19:37 +0100

    Create string table with strings from other files

--- constants.hpp
+++ constants.hpp
@@ -1,13 +1,118 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 
-#define BST_UNCHECKED       0x0000
-#define BST_CHECKED         0x0001
+#define BST_UNCHECKED                   0x0000
+#define BST_CHECKED                     0x0001
 
-#define MATCH_VERSION   "1.0"
-#define USER_AGENT      "SimpleNoter/0.52"
-#define INI_FILE        "config.ini"
-#define APPNAME         "Simple Noter v0.52"
-#define HELPFILE        "SNOTER05.HLP"
+#define MATCH_VERSION                   "1.0"
+#define USER_AGENT                      "SimpleNoter/0.52"
+#define INI_FILE                        "config.ini"
+#define APPNAME                         "Simple Noter v0.52"
+#define HELPFILE                        "SNOTER05.HLP"
+
+// --------------------------------------------------------------
+// STRING TABLES
+// --------------------------------------------------------------
+
+// Polish string table
+
+#define STRING_NEW_NOTE                 "~ Nowa notatka ~ - "
+#define STRING_WRONG_RESPONSE           "Nie uda³o siê po³¹czyæ z serwerem."
+#define STRING_SERVICE_DISABLED         "Us³uga tymczasowo niedostêpna."
+#define STRING_INTERNAL_SERVER_ERROR    "B³¹d wewnêtrzny serwera."
+#define STRING_NOTE_ALREADY_UNLOCKED    "Notatka ju¿ odblokowana."
+#define STRING_NOTE_ALREADY_LOCKED      "Notatka ju¿ zablokowana."
+#define STRING_NOTE_LOCKED              "Notatka jest zablokowana."
+#define STRING_USER_REMOVAL_FAILURE     "Usuniêcie u¿ytkownika nie powiod³o siê."
+#define STRING_USER_NOT_EXISTS          "Dany u¿ytkownik nie istnieje."
+#define STRING_NOTE_NOT_EXISTS          "Dana notatka nie istnieje."
+#define STRING_NO_NECESSARY_INFORMATION "Brak wymaganych informacji (temat lub treœæ)."
+#define STRING_USER_DEACTIVATED         "U¿ytkownik zablokowany."
+#define STRING_LOGIN_INCORRECT          "Nieprawid³owe dane logowania."
+#define STRING_UNKNOWN_ACTION           "Nieprawid³owe polecenie."
+#define STRING_NO_CREDENTIALS           "Brak danych logowania."
+#define STRING_USER_EXISTS              "Podana nazwa u¿ytkownika jest ju¿ zajêta."
+#define STRING_NO_USABLE_INFORMATION    "Brak u¿ytecznych informacji w ¿¹daniu."
+#define STRING_INVALID_METHOD           "Nieobs³ugiwane ¿¹danie."
+#define STRING_INFO_OK                  "Wszystko w porz¹dku."
+#define STRING_INFO_USER_CREATED        "U¿ytkownik zosta³ zarejestrowany."
+#define STRING_INFO_USER_UPDATED        "Dane u¿ytkownika zosta³y zaktualizowane."
+#define STRING_INFO_USER_REMOVED        "U¿ytkownik zosta³ usuniêty."
+#define STRING_INFO_LIST_SUCCESSFUL     "Lista notatek zosta³a za³adowana."
+#define STRING_INFO_NOTE_RETRIEVED      "Notatka zosta³a za³adowana."
+#define STRING_INFO_NOTE_CREATED        "Notatka zosta³a utworzona."
+#define STRING_INFO_NOTE_UPDATED        "Notatka zosta³a zaktualizowana."
+#define STRING_INFO_NOTE_DELETED        "Notatka zosta³a usuniêta."
+#define STRING_INFO_USER_INFO_RETRIEVED "Informacje o u¿ytkowniku zosta³y za³adowane."
+#define STRING_INFO_NOTE_LOCKED         "Notatka zosta³a zablokowana."
+#define STRING_INFO_NOTE_UNLOCKED       "Notatka zosta³a odblokowana."
+#define STRING_UNKNOWN_ERROR            "Nieznany kod odpowiedzi."
+
+#define STRING_ADD                      "Dodaj"
+#define STRING_UPDATE                   "Aktualizuj"
+#define STRING_PROPERTIES               "W³aœciwoœci"
+#define STRING_CLOSE                    "Zamknij"
+#define STRING_INFORMATION              "Informacja"
+#define STRING_WARNING                  "Ostrze¿enie"
+#define STRING_ERROR                    "B³¹d"
+#define STRING_DOWNLOAD                 "Pobierz"
+#define STRING_CREATE                   "Utwórz"
+#define STRING_OPEN                     "Otwórz"
+#define STRING_DELETE                   "Usuñ"
+#define STRING_EXIT                     "Zakoñcz"
+
+#define STRING_NORMAL_WINDOW            "Normalne okno"
+#define STRING_MINIMIZED_WINDOW         "Zminimalizowane"
+#define STRING_MAXIMIZED_WINDOW         "Zmaksymalizowane"
+
+#define STRING_ID                       "ID:"
+#define STRING_LAST_CHANGED             "Ostatnie zmiany:"
+#define STRING_NOT_CHOSEN               "-- nie wybrano --"
+#define STRING_NOT_CONNECTED            "-- nie po³¹czono --"
+#define STRING_NOT_LOGGED_IN            "-- nie zalogowano --"
+#define STRING_SPACED_COUNT             " Iloœæ: "
+#define STRING_SPACED_LAST_MOD_DATE     " Data ostatniej modyfikacji: "
+
+#define STRING_LOADING_NOTE_LIST        "Pobieranie listy notatek..."
+#define STRING_CREATING_EDIT_WINDOW     "Tworzenie okna edycji..."
+#define STRING_DOWNLOADING_NOTE         "Pobieranie notatki..."
+#define STRING_TO_NEW_NOTE              "Przekierowano na now¹ notatkê."
+
+#define STRING_MENU_ADD                 "Dodaj\tCtrl+S"
+#define STRING_MENU_UPDATE              "Aktualizuj\tCtrl+S"
+
+#define STRING_EDITWIN_TITLE            "Tytu³:"
+#define STRING_EDITWIN_ENTRY            "Treœæ:"
+#define STRING_EDITWIN_CREATE_ERROR     "Nie uda³o siê utworzyæ okna edycji."
+
+#define STRING_MSG_WANT_CHANGES_SAVED   "Czy chcesz zapisaæ zmiany?"
+#define STRING_MSG_CTL3D_ERROR          "Zarejestrowanie CTL3D nie powiod³o siê!"
+#define STRING_MSG_UNSUPPORTED_CHARS    "Wybrana notatka zawiera nieobs³ugiwane znaki UTF-8.\nDokonywanie zmian t¹ wersj¹ programu spowoduje nieodwracaln¹ utratê tych informacji."
+#define STRING_MSG_WINSOCK_ERROR        "Inicjalizacja sk³adnika WinSock nie powiod³a siê. Program zostanie zamkniêty."
+#define STRING_MSG_WNDCLASS_ERROR       "Utworzenie klasy okna nie powiod³o siê. Program zostanie zamkniêty."
+#define STRING_MSG_WND_CREATE_ERROR     "Utworzenie okna nie powiod³o siê. Program zostanie zamkniêty."
+#define STRING_MSG_ACCELERATORS_ERROR   "Nie uda³o siê za³adowaæ akceleratorów! Program zostanie zamkniêty."
+#define STRING_MSG_WANT_RELOAD          "Czy chcesz prze³adowaæ listê notatek?"
+#define STRING_MSG_WANT_NOTE_REMOVAL    "Czy na pewno chcesz usun¹æ notatkê"
+#define STRING_MSG_CTL3D_UNREG_ERROR    "Wyrejestrowanie aplikacji z CTL3D nie powiod³o siê!"
+#define STRING_MSG_CTL3D_CHANGE         "Aby zmiany ustawieñ kontrolek 3D odnios³y skutek, wymagane jest ponowne uruchomienie programu."
+#define STRING_MSG_WRONG_PORT_NUMBER    "Nieprawid³owy numer portu."
+#define STRING_MSG_HOST_NOT_FOUND       "Nie uda³o siê odnaleŸæ hosta."
+#define STRING_MSG_CONN_ESTABLISHED     "Uda³o siê prawid³owo zestawiæ po³¹czenie."
+#define STRING_MSG_CONNECTION_ERROR     "Po³¹czenie nie powiod³o siê."
+#define STRING_MSG_CREDENTIALS_CHANGED  "Dane logowania uleg³y zmianie na serwerze.\nCzy na pewno chcesz porzuciæ zmiany?"
+#define STRING_MSG_LOGIN_SUCCESSFUL     "Uda³o siê prawid³owo zalogowaæ."
+#define STRING_MSG_NO_CONN_SETTINGS     "Brak ustawieñ po³¹czenia z serwerem."
+#define STRING_MSG_ACC_DELETE_PART1     "Czy na pewno chcesz usun¹æ konto "
+#define STRING_MSG_ACC_DELETE_PART2     "?\nKontynuacja usunie wszystkie notatki oraz informacje o u¿ytkowniku przechowywane na serwerze.\nOperacja ta jest nieodwracalna!!!\n\nCzy na pewno chcesz usun¹æ swoje konto?"
+#define STRING_MSG_REGISTRATION_SUCC    "Uda³o siê prawid³owo zarejestrowaæ nowego u¿ytkownika."
+#define STRING_MSG_REGISTRATION_ERROR   "Nie uda³o siê zarejestrowaæ nowego u¿ytkownika.\n"
+#define STRING_MSG_PASSWORDS_NO_MATCH   "Has³a nie s¹ takie same!"
+#define STRING_MSG_USER_SPACED          "U¿ytkownik "
+#define STRING_MSG_USER_DELETED_SPACED  " zosta³ usuniêty."
+#define STRING_MSG_WRONG_PASSWORD       "Nieprawid³owe has³o!"
+#define STRING_MSG_PASSWORD_CHANGED     "Has³o zosta³o zmienione."
+
+// End of Polish string table
 
 #endif
