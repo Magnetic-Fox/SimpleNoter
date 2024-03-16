bb5b6ef56adcb1966bb2b6e3c8048edb2065b09c
Bartłomiej "Magnetic-Fox" Węgrzyn <magnetic-fox@hotmail.com>
2024-03-16 15:22:51 +0100

    Update user agent and version information

--- constants.hpp
+++ constants.hpp
@@ -7,9 +7,9 @@
 #define BST_CHECKED                     0x0001
 
 #define MATCH_VERSION                   "1.0"
-#define USER_AGENT                      "SimpleNoter/0.52"
+#define USER_AGENT                      "SimpleNoter/0.6"
 #define INI_FILE                        "config.ini"
-#define APPNAME                         "Simple Noter v0.52"
-#define HELPFILE                        "SNOTER05.HLP"
+#define APPNAME                         "Simple Noter v0.6"
+#define HELPFILE                        "SNOTER06.HLP"
 
 #endif
