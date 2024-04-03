51ee35871111c1fa133491decfc2770e3e6ac994
Bartłomiej "Magnetic-Fox" Węgrzyn <magnetic-fox@hotmail.com>
2024-04-04 00:21:43 +0200

    Move user agent and program version info to resources

--- constants.hpp
+++ constants.hpp
@@ -1,15 +1,13 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 
-#include "string_table.hpp"
-
 #define BST_UNCHECKED                   0x0000
 #define BST_CHECKED                     0x0001
 
 #define MATCH_VERSION                   "1.0"
-#define USER_AGENT                      "SimpleNoter/0.68"
+//#define USER_AGENT                      "SimpleNoter/0.7"
 #define INI_FILE                        "config.ini"
-#define APPNAME                         "Simple Noter v0.68"
+//#define APPNAME                         "Simple Noter v0.7"
 #define HELPFILE                        "SNOTER06.HLP"
 
 #endif
