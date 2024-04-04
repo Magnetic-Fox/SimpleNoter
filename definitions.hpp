ec9642207950b07b6b5f024cc5d08e7a409066eb
Bartłomiej "Magnetic-Fox" Węgrzyn <magnetic-fox@hotmail.com>
2024-04-04 18:25:30 +0200

    Move some controls IDs from definitions.hpp to constants.hpp

--- definitions.hpp
+++ definitions.hpp
@@ -5,29 +5,6 @@
 #include "noteprocs.hpp"
 #include "libdefs.hpp"
 
-#define ID_BUTTON1                      1400
-#define ID_BUTTON2                      1401
-#define ID_BUTTON3                      1402
-#define ID_BUTTON4                      1403
-#define ID_BUTTON5                      1404
-#define ID_LISTBOX                      1500
-#define ID_STATIC1                      1600
-#define ID_STATIC2                      1601
-#define ID_STATIC3                      1602
-#define ID_STATIC4                      1603
-#define ID_STATIC5                      1604
-#define ID_STATIC6                      1605
-
-#define ID_EDIT_BUTTON1                 2400
-#define ID_EDIT_BUTTON2                 2401
-#define ID_EDIT_BUTTON3                 2402
-#define ID_EDIT_EDITBOX1                2500
-#define ID_EDIT_EDITBOX2                2501
-#define ID_EDIT_STATIC1                 2600
-#define ID_EDIT_STATIC2                 2601
-#define ID_EDIT_STATIC3                 2602
-#define ID_EDIT_STATIC4                 2603
-
 typedef struct editWindow {
     HWND hwnd;
     HWND hStatic, hStatic2, hStatic3, hStatic4;
