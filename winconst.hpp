8745d2c950558ce2c581d081ce65262d784a9403
Bartłomiej "Magnetic-Fox" Węgrzyn <magnetic-fox@hotmail.com>
2024-04-12 18:45:34 +0200

    Move typical Windows constants to separate file

---
+++ winconst.hpp
@@ -0,0 +1,12 @@
+#ifndef WINCONST_H
+#define WINCONST_H
+
+#define BST_UNCHECKED   0x0000
+#define BST_CHECKED     0x0001
+
+#define WC_BUTTON       "Button"
+#define WC_EDIT         "Edit"
+#define WC_LISTBOX      "ListBox"
+#define WC_STATIC       "Static"
+
+#endif
