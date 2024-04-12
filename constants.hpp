25b725c22abb079255370005ea8b43a1ee3b726e
Bartłomiej "Magnetic-Fox" Węgrzyn <magnetic-fox@hotmail.com>
2024-04-12 18:45:52 +0200

    Move typical Windows constants to winconst.hpp file

--- constants.hpp
+++ constants.hpp
@@ -1,41 +1,33 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 
-#define BST_UNCHECKED                   0x0000
-#define BST_CHECKED                     0x0001
+#define ID_BUTTON1          1400
+#define ID_BUTTON2          1401
+#define ID_BUTTON3          1402
+#define ID_BUTTON4          1403
+#define ID_BUTTON5          1404
+#define ID_LISTBOX          1500
+#define ID_STATIC1          1600
+#define ID_STATIC2          1601
+#define ID_STATIC3          1602
+#define ID_STATIC4          1603
+#define ID_STATIC5          1604
+#define ID_STATIC6          1605
 
-#define WC_BUTTON                       "Button"
-#define WC_EDIT                         "Edit"
-#define WC_LISTBOX                      "ListBox"
-#define WC_STATIC                       "Static"
+#define ID_EDIT_BUTTON1     2400
+#define ID_EDIT_BUTTON2     2401
+#define ID_EDIT_BUTTON3     2402
+#define ID_EDIT_EDITBOX1    2500
+#define ID_EDIT_EDITBOX2    2501
+#define ID_EDIT_STATIC1     2600
+#define ID_EDIT_STATIC2     2601
+#define ID_EDIT_STATIC3     2602
+#define ID_EDIT_STATIC4     2603
 
-#define ID_BUTTON1                      1400
-#define ID_BUTTON2                      1401
-#define ID_BUTTON3                      1402
-#define ID_BUTTON4                      1403
-#define ID_BUTTON5                      1404
-#define ID_LISTBOX                      1500
-#define ID_STATIC1                      1600
-#define ID_STATIC2                      1601
-#define ID_STATIC3                      1602
-#define ID_STATIC4                      1603
-#define ID_STATIC5                      1604
-#define ID_STATIC6                      1605
+#define MATCH_VERSION       "1.0"
+#define INI_FILE            "config.ini"
 
-#define ID_EDIT_BUTTON1                 2400
-#define ID_EDIT_BUTTON2                 2401
-#define ID_EDIT_BUTTON3                 2402
-#define ID_EDIT_EDITBOX1                2500
-#define ID_EDIT_EDITBOX2                2501
-#define ID_EDIT_STATIC1                 2600
-#define ID_EDIT_STATIC2                 2601
-#define ID_EDIT_STATIC3                 2602
-#define ID_EDIT_STATIC4                 2603
-
-#define MATCH_VERSION                   "1.0"
-#define INI_FILE                        "config.ini"
-
-#define NOTER_MAINWINDOW                "SimpleNoterMain"
-#define NOTER_EDITWINDOW                "SimpleNoterEdit"
+#define NOTER_MAINWINDOW    "SimpleNoterMain"
+#define NOTER_EDITWINDOW    "SimpleNoterEdit"
 
 #endif
