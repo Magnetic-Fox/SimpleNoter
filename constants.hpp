9b7e44435e58e0a26ca00ecdfbd680acfad0e4d3
Bartłomiej "Magnetic-Fox" Węgrzyn <magnetic-fox@hotmail.com>
2024-12-31 13:41:25 +0100

    Move action strings to constants.hpp

--- constants.hpp
+++ constants.hpp
@@ -1,36 +1,50 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 
-#define IDB_DOWNLOAD        1400
-#define IDB_CREATE          1401
-#define IDB_OPEN            1402
-#define IDB_EXIT            1403
-#define IDB_DELETE          1404
-#define ID_LISTBOX          1500
-#define IDC_SID             1600
-#define IDC_LASTCHANGED     1601
-#define IDC_NOTEID          1602
-#define IDC_NOTELASTMOD     1603
-#define IDC_STATUS          1604
-#define IDC_GRAYBOX         1605
+#define IDB_DOWNLOAD            1400
+#define IDB_CREATE              1401
+#define IDB_OPEN                1402
+#define IDB_EXIT                1403
+#define IDB_DELETE              1404
+#define ID_LISTBOX              1500
+#define IDC_SID                 1600
+#define IDC_LASTCHANGED         1601
+#define IDC_NOTEID              1602
+#define IDC_NOTELASTMOD         1603
+#define IDC_STATUS              1604
+#define IDC_GRAYBOX             1605
 
-#define IDB_EDIT_ADDUP      2400
-#define IDB_EDIT_PROPERTIES 2401
-#define IDB_EDIT_CLOSE      2402
-#define IDE_EDIT_SUBJECT    2500
-#define IDE_EDIT_ENTRY      2501
-#define IDC_EDIT_SUBJECT    2600
-#define IDC_EDIT_ENTRY      2601
-#define IDC_EDIT_GRAYBOX    2602
-#define IDC_EDIT_STATUS     2603
+#define IDB_EDIT_ADDUP          2400
+#define IDB_EDIT_PROPERTIES     2401
+#define IDB_EDIT_CLOSE          2402
+#define IDE_EDIT_SUBJECT        2500
+#define IDE_EDIT_ENTRY          2501
+#define IDC_EDIT_SUBJECT        2600
+#define IDC_EDIT_ENTRY          2601
+#define IDC_EDIT_GRAYBOX        2602
+#define IDC_EDIT_STATUS         2603
 
-#define MATCH_VERSION       "1.0"
-#define INI_FILE            "config.ini"
+#define MATCH_VERSION           "1.0"
+#define INI_FILE                "config.ini"
 
-#define NOTER_MAINWINDOW    "SimpleNoterMain"
-#define NOTER_EDITWINDOW    "SimpleNoterEdit"
+#define NOTER_MAINWINDOW        "SimpleNoterMain"
+#define NOTER_EDITWINDOW        "SimpleNoterEdit"
 
-#define GET_METHOD          "GET"
-#define POST_METHOD         "POST"
+#define GET_METHOD              "GET"
+#define POST_METHOD             "POST"
+
+#define BZ_COMPRESSED_HEADER    "X-BZ-Compressed"
+#define TRUE_ANSWER             "yes"
+#define LIST_ACTION             "list"
+#define RETRIEVE_ACTION         "retrieve"
+#define INFO_ACTION             "info"
+#define LOCK_ACTION             "lock"
+#define UNLOCK_ACTION           "unlock"
+#define DELETE_ACTION           "delete"
+#define ADD_ACTION              "add"
+#define UPDATE_ACTION           "update"
+#define REGISTER_ACTION         "register"
+#define CHANGE_ACTION           "change"
+#define REMOVE_ACTION           "remove"
 
 #endif
